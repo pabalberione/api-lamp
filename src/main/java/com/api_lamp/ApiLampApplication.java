c69bea76458c01fa7f6a3a03bf61b67769d0aa3b
Pabalberione <pabalberione@gmail.com>
2024-09-16 09:06:48 -0300

    First commit

---
+++ src/main/java/com/api_lamp/ApiLampApplication.java
@@ -0,0 +1,13 @@
+package com.api_lamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_lamp/ApiLampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_lamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
